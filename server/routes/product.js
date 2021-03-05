2bf09889f0439e55ee6d5262f2563262ee5869b6
billowycloud <bjkim0228@naver.com>
2021-03-05 13:49:43 +0900

    Add Image Multer

---
+++ server/routes/product.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const multer = require('multer');
+
+const router = express.Router();
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+var upload = multer({ storage: storage }).single('file');
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image', (req, res) => {
+  // 가져온 이미지 저장하기
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+module.exports = router;
